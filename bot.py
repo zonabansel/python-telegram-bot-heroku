44db79e17764f2b9bc69a252337de90b6e0f8d9a
zonabansel <114482374+zonabansel@users.noreply.github.com>
2022-09-28 15:29:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = '5671251738:AAEbu6j7PXohij75EWQ4JKmRPxzE3DTwmf8'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -53,7 +53,7 @@ def main():
         listen="0.0.0.0",
         port=int(PORT),
         url_path=TOKEN,
-        webhook_url='https://yourherokuappname.herokuapp.com/' + TOKEN
+        webhook_url='https://zonabansel.osc-fr1.scalingo.io/' + TOKEN
     )
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
